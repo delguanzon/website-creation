20c1a638d723bac8fbca1e6562167ef279d24d24
delguanzon <yodelguanzon@gmail.com>
2022-10-24 09:16:59 -0700

    Initial commit
    Co-authored-by: Alejandro Socarras <apsocarras@gmail.com>
    Co-authored-by: Celeste Grobey <celestegrobey@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+window.addEventListener(load, function() {
+
+  const body = document.querySelector("body");
+  const p = document.createElement("p");
+  const h1 = document.createElement("h1");
+  const h2 = document.createElement("h2");
+  const img = document.createElement("img");
+  const ul = document.createElement("ul");
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  
+
+});
